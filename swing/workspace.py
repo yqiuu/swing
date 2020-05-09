2f76347501662c41022ad3453cf3f3e334beb1a4
yqiuu <yishengq@student.unimelb.edu.au>
2020-05-09 19:14:37 +1000

    Rearrange method positions

--- swing/workspace.py
+++ swing/workspace.py
@@ -63,46 +63,6 @@ class Workspace:
             + ['_rstate', '_pos_global_best', '_cost_global_best', '_memo']
 
 
-    def _update_memo(self):
-        self._memo['iter'].append(self._i_iter)
-        self._memo['ncall'].append(self._ncall)
-        self._memo['pos'].append(self._pos_global_best)
-        self._memo['cost'].append(self._cost_global_best)
-
-
-    def _evaluate(self, pos):
-        self._ncall += 1
-        return self._func(pos)
-
-
-    def _evaluate_multi(self, pos):
-        self._ncall += len(pos)
-        if self._pool is None:
-            return np.asarray(list(map(self._func, pos)))
-        else:
-            return np.asarray(list(self._pool.map(self._func, pos)))
-
-
-    def _update_global_best(self):
-        idx_min = np.argmin(self._cost)
-        cost_min = self._cost[idx_min]
-        if cost_min < self._cost_global_best:
-            self._pos_global_best = np.copy(self._pos[idx_min])
-            self._cost_global_best = cost_min
-
-
-    def _init_new_pos(self):
-        return self._lbounds + self._dbounds*self._rstate.rand(self._ndim)
-
-
-    def _phase_init(self):
-        return {'init': {'pos': None, 'cost': None}}
-
-
-    def _phase_main(self):
-        return {'main': {'pos': None, 'cost': None}}
-
-
     def swarm(self, niter=100):
         """Run the optimizer.
 
@@ -164,3 +124,43 @@ class Workspace:
         for key in self._restart_keys:
             checkpoint[key] = getattr(self, key)
         pickle.dump(checkpoint, open(fname, 'wb'))
+
+
+    def _update_memo(self):
+        self._memo['iter'].append(self._i_iter)
+        self._memo['ncall'].append(self._ncall)
+        self._memo['pos'].append(self._pos_global_best)
+        self._memo['cost'].append(self._cost_global_best)
+
+
+    def _evaluate(self, pos):
+        self._ncall += 1
+        return self._func(pos)
+
+
+    def _evaluate_multi(self, pos):
+        self._ncall += len(pos)
+        if self._pool is None:
+            return np.asarray(list(map(self._func, pos)))
+        else:
+            return np.asarray(list(self._pool.map(self._func, pos)))
+
+
+    def _update_global_best(self):
+        idx_min = np.argmin(self._cost)
+        cost_min = self._cost[idx_min]
+        if cost_min < self._cost_global_best:
+            self._pos_global_best = np.copy(self._pos[idx_min])
+            self._cost_global_best = cost_min
+
+
+    def _init_new_pos(self):
+        return self._lbounds + self._dbounds*self._rstate.rand(self._ndim)
+
+
+    def _phase_init(self):
+        return {'init': {'pos': None, 'cost': None}}
+
+
+    def _phase_main(self):
+        return {'main': {'pos': None, 'cost': None}}
