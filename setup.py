9f73a8c2456639198603c91d718ffd320bd7ba9d
邱逸盛 <yishengq@zhejianglab.com>
2024-06-14 15:08:02 +0800

    chore: Update setup.py and version number

--- setup.py
+++ setup.py
@@ -4,10 +4,20 @@ from setuptools import setup
 # Get version
 exec(open('swing/version.py', 'r').read())
 #
+description = """Swarm intelligence optimization."""
+
+install_requires = [
+    'numpy>=1.17',
+    'tqdm',
+]
+
 setup(
-    name='swing',
+    name='swing-opt',
     version=__version__,
     author='Yisheng Qiu',
+    description=description,
+    url='https://github.com/yqiuu/swing',
+    install_requires=install_requires,
     license='MIT',
     packages=['swing'],
 )
